MotorolaMobilityLLC/kernel-msm

msm: camera: mem: Remove lock from get_cpu_buf unless mapping
This change removes mutex_lock/unlock from get_cpu_buf function
unless mapping is required. This helps to remove lock from most
calls to this function. There are no checks anyway to see if
buffer is still in use or not before release. The alloc/map/release
calls have a mutex lock when modifying the different parameters
of the bufq struct. Having a mutex lock in this function does not
resolve anything. So removing this helps to be able to call this
function from tasklets as long as mapping is already done.

Change-Id: I48b195ad5c948ce991d8926538d6f61c2e0eef59
Signed-off-by: Harsh Shah <harshs@codeaurora.org>

drivers/media/platform/msm/camera/cam_req_mgr/cam_mem_mgr.c
@@ -244,37 +244,42 @@ int cam_mem_get_cpu_buf(int32_t buf_handle, uint64_t *vaddr_ptr, size_t *len)
 	if (!tbl.bufq[idx].active)
 		return -EPERM;
 
-	mutex_lock(&tbl.bufq[idx].q_lock);
 	if (buf_handle != tbl.bufq[idx].buf_handle) {
 		rc = -EINVAL;
 		goto exit_func;
 	}
 
-	dmabuf = tbl.bufq[idx].dma_buf;
-	if (!dmabuf) {
-		CAM_ERR(CAM_MEM, "Invalid DMA buffer pointer");
+	if (!(tbl.bufq[idx].flags & CAM_MEM_FLAG_KMD_ACCESS)) {
 		rc = -EINVAL;
 		goto exit_func;
 	}
 
-	if (tbl.bufq[idx].flags & CAM_MEM_FLAG_KMD_ACCESS) {
-		if (!tbl.bufq[idx].kmdvaddr) {
-			rc = cam_mem_util_map_cpu_va(dmabuf,
-				&kvaddr, &klen);
-			if (rc)
-				goto exit_func;
-			tbl.bufq[idx].kmdvaddr = kvaddr;
+	if (!tbl.bufq[idx].kmdvaddr) {
+		mutex_lock(&tbl.bufq[idx].q_lock);
+		dmabuf = tbl.bufq[idx].dma_buf;
+		if (!dmabuf) {
+			CAM_ERR(CAM_MEM, "Invalid DMA buffer pointer");
+			rc = -EINVAL;
+			goto release_mutex;
 		}
-	} else {
-		rc = -EINVAL;
-		goto exit_func;
+
+		rc = cam_mem_util_map_cpu_va(dmabuf,
+			&kvaddr, &klen);
+		if (rc)
+			goto release_mutex;
+
+		tbl.bufq[idx].kmdvaddr = kvaddr;
+		mutex_unlock(&tbl.bufq[idx].q_lock);
 	}
 
 	*vaddr_ptr = tbl.bufq[idx].kmdvaddr;
 	*len = tbl.bufq[idx].len;
 
-exit_func:
+	return rc;
+
+release_mutex:
 	mutex_unlock(&tbl.bufq[idx].q_lock);
+exit_func:
 	return rc;
 }
 EXPORT_SYMBOL(cam_mem_get_cpu_buf);

pixeldustproject-p/android_kernel_google_bluecross

function from tasklets as long as mapping is already done. This
change is needed to dump ISP packet on error from tasklet.
(cherry picked from commit c7fa850d9654df41a89896525caf73df06018dc2)

drivers/media/platform/msm/camera/cam_req_mgr/cam_mem_mgr.c
@@ -201,37 +201,42 @@ int cam_mem_get_cpu_buf(int32_t buf_handle, uintptr_t *vaddr_ptr, size_t *len)
 	if (!tbl.bufq[idx].active)
 		return -EPERM;
 
-	mutex_lock(&tbl.bufq[idx].q_lock);
 	if (buf_handle != tbl.bufq[idx].buf_handle) {
 		rc = -EINVAL;
 		goto exit_func;
 	}
 
-	ion_hdl = tbl.bufq[idx].i_hdl;
-	if (!ion_hdl) {
-		CAM_ERR(CAM_MEM, "Invalid ION handle");
+	if (!(tbl.bufq[idx].flags & CAM_MEM_FLAG_KMD_ACCESS)) {
 		rc = -EINVAL;
 		goto exit_func;
 	}
 
-	if (tbl.bufq[idx].flags & CAM_MEM_FLAG_KMD_ACCESS) {
-		if (!tbl.bufq[idx].kmdvaddr) {
-			rc = cam_mem_util_map_cpu_va(ion_hdl,
-				&kvaddr, &klen);
-			if (rc)
-				goto exit_func;
-			tbl.bufq[idx].kmdvaddr = kvaddr;
+	if (!tbl.bufq[idx].kmdvaddr) {
+		mutex_lock(&tbl.bufq[idx].q_lock);
+		ion_hdl = tbl.bufq[idx].i_hdl;
+		if (!ion_hdl) {
+			CAM_ERR(CAM_MEM, "Invalid ION handle");
+			rc = -EINVAL;
+			goto release_mutex;
 		}
-	} else {
-		rc = -EINVAL;
-		goto exit_func;
+
+		rc = cam_mem_util_map_cpu_va(ion_hdl,
+			&kvaddr, &klen);
+		if (rc)
+			goto release_mutex;
+
+		tbl.bufq[idx].kmdvaddr = kvaddr;
+		mutex_unlock(&tbl.bufq[idx].q_lock);
 	}
 
 	*vaddr_ptr = tbl.bufq[idx].kmdvaddr;
 	*len = tbl.bufq[idx].len;
 
-exit_func:
+	return rc;
+
+release_mutex:
 	mutex_unlock(&tbl.bufq[idx].q_lock);
+exit_func:
 	return rc;
 }
 EXPORT_SYMBOL(cam_mem_get_cpu_buf);